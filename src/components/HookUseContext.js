d40c93ea1599fae6ea95b8cf65643eae0b78c3ab
Rafael Conrado de Souza <rafconradoo@gmail.com>
2024-06-18 23:02:44 -0300

    Create HookUseContext.js

---
+++ src/components/HookUseContext.js
@@ -0,0 +1,12 @@
+import { Children, createContext } from "react";
+
+export const SomeContext = createContext();
+
+export const HookUseContext = ({ children }) => {
+    
+    const contextValue = "texting context"
+
+    return
+        
+    
+};
