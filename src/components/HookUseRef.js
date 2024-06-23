6299aade420e29706d637ac155940f81fae80225
Rafael Conrado de Souza <rafconradoo@gmail.com>
2024-06-23 12:52:12 -0300

    att

--- src/components/HookUseRef.js
+++ src/components/HookUseRef.js
@@ -6,14 +6,37 @@ const HookUseRef = () => {
   const [counter, setCounter] = useState(0);
   const [counterB, setCounterB] = useState(0);
 
+  useEffect(() => {
+    numberRef.current = numberRef.current + 1;
+  });
+
+  //2- useRef and DOM
+  const inputRef = useRef();
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <h2>useRef</h2>
+      {/*1- useRef */}
       <p>O componente renderizou: {numberRef.current} vezes.</p>
       <p>Counter 1: {counter}</p>
       <button onClick={() => setCounter(counter + 1)}>Contador A</button>
       <p>Counter 1: {counterB}</p>
       <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
+      {/*2- useRef and DOM */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={inputRef}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
       <hr />
     </div>
   );
