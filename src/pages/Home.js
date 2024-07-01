18ffc15e95e8d63aecc496a939847eff0ac10051
Rafael Conrado de Souza <rafconradoo@gmail.com>
2024-06-30 22:05:04 -0300

    att

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,13 @@ import HookUseReducer from "../components/HookUseReducer";
 import HooksUseEffect from "../components/HooksUseEffect";
 import HookUseContext from "../components/HookUseContext";
 import HookUseRef from "../components/HookUseRef";
+import HookUseMemo from "../components/HookUseMemo";
+import HookUseLayoutEffect from "../components/HookUseLayoutEffect";
 
 //useContext
 import { useContext } from "react";
 import { SomeContext } from "../components/HookUseContext";
-import HookUseMemo from "../components/HookUseMemo";
+import HookCustom from "../components/HookCustom";
 
 const Home = () => {
   const { contextValue } = useContext(SomeContext);
@@ -24,6 +26,8 @@ const Home = () => {
       <hr />
       <HookUseRef />
       <HookUseMemo />
+      <HookUseLayoutEffect />
+      <HookCustom />
     </div>
   );
 };
